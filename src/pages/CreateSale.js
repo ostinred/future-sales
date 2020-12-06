e7ff8f80789eaa6699197d29b7357508a6c88156
Andrii Doroshev <ostinred@gmail.com>
2020-12-06 12:12:46 +0200

    changes

--- src/pages/CreateSale.js
+++ src/pages/CreateSale.js
@@ -53,6 +53,7 @@ const CreateSale = () => {
       variants={ PAGE_VARIANT_BASIC }>
       <Header />
       <Layout classNamePage="create-sale-page">
+<<<<<<< HEAD
         <form onSubmit={ handleSubmit(onSubmit) } >
           <div className="whiteBgc">
             <div className="section">
@@ -93,6 +94,32 @@ const CreateSale = () => {
             Post Now
           </button>
         </form>
+=======
+        <div className="section">
+          <div className="photo">
+            <h2>Add Photo</h2>
+            <div className="addPhotoContainer">
+              <img src="./add.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <h2>Product</h2>
+          <input className="input" type="text" placeholder="e.g. Iphone" />
+        </div>
+        <div className="section">
+          <h2>Buying price</h2>
+          <input className="input" type="text" placeholder="$" />
+        </div>
+        <div className="section">
+          <h2>Selling price</h2>
+          <input className="input" type="text" placeholder="$" />
+        </div>
+        <div className="section">
+          <h2>Selling date</h2>
+          <input className="input" type="text" placeholder="After 3 month" />
+        </div>
+>>>>>>> changes
       </Layout>
     </motion.div>
   );
